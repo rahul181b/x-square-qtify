73470b43fe44fef6747b59fcdddde199b00259f8
Rahul <bhaleraor17@gmail.com>
2024-02-24 19:22:44 +0530

    logo

--- x-square-qtify/qtify/src/components/Logo/Logo.jsx
+++ x-square-qtify/qtify/src/components/Logo/Logo.jsx
@@ -3,5 +3,9 @@ import React from "react";
 import styles from "./Logo.module.css";
 
 export default function Logo() {
-   return <img className={styles.logo} src={LogoImg} alt="logo " width={67} />;
+   return (
+      <div className={styles.logoContainer}>
+         <img className={styles.logo} src={LogoImg} alt="logo " width={67} />
+      </div>
+   );
 }
